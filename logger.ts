98e15e512ffdb026bddeafb718f71e9f80d0613a
Denis Kushnir <avoid3301@gmail.com>
2025-01-28 18:52:03 +0200

    change logs folder for production

--- logger.ts
+++ logger.ts
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === "production") {
         {
             target: "pino-pretty",
             options: {
-                destination: "~/NewsBot/logs.txt",
+                destination: "~/Logs/NewsBot/logs.txt",
             }
         },
     ]
